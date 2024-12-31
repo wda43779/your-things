8206e34dea232d7b11c6d3e1b5083c8b88b0cb29
wda43779 <wda43779@gmail.com>
2024-12-31 15:52:25 +0800

    update create db scripts

--- python-backend/createdb.py
+++ python-backend/createdb.py
@@ -2,15 +2,40 @@ import sqlite3
 import os
 from constants import DB_PATH 
 
-SQL_FILE_PATH = 'ddl.sql'
+ddl = """
+-- Filename: script.sql
+-- Author: wjw
+-- Description: data base data definition language
+CREATE TABLE files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    file_type VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    tag JSON
+);
+CREATE TABLE words (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    word VARCHAR(255) NOT NULL
+);
+CREATE TABLE re_sort (
+    file_id INT,
+    word_id INT,
+    PRIMARY KEY (file_id, word_id)
+);
+"""
 
 def create_db():
     con = sqlite3.connect(DB_PATH)
+    print(DB_PATH)
     cur = con.cursor()
-    with open(SQL_FILE_PATH, 'r') as file:
-        sql_script = file.read()
-        sql_statements = [statement.strip() for statement in sql_script.split(';') if statement.strip()]
-        for statement in sql_statements:
-            cur.execute(statement)
+    sql_script = ddl
+    sql_statements = [statement.strip() for statement in sql_script.split(';') if statement.strip()]
+    for statement in sql_statements:
+        cur.execute(statement)
     con.commit()
     con.close()
+
+if __name__ == "__main__":
+    create_db()

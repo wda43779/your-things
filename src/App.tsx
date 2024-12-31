5aacaabda3034110a2ae3212055630c2704d37b5
wda43779 <wda43779@gmail.com>
2024-12-31 09:56:46 +0800

    add loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,21 @@ function App() {
 
   const [searchResult, $searchResult] = useState<FileResult[]>([]);
 
-  const [searchProcess, $searchProcess] = useState<string>("");
+  // const [searchProcess, $searchProcess] = useState<string>("");
+  const [searching, $searching] = useState<boolean>(false);
 
   const handleSearch: React.MouseEventHandler<HTMLFormElement> = async (ev) => {
     ev.preventDefault();
-    let res = await window.electronAPI.searchByFilename(".", text);
-    console.log("ipc", res);
+    $searching(true);
+    let res: FileResult[] = [];
+    try {
+      res = await window.electronAPI.searchByFilename(".", text);
+    } catch (error) {
+      console.log("error searching", error);
+    } finally {
+      $searching(false);
+    }
+
     $searchResult(res);
   };
 
@@ -34,8 +43,10 @@ function App() {
       <form onSubmit={handleSearch}>
         <input value={text} onChange={(ev) => $text(ev.target.value)}></input>
         <button type="submit">搜索</button>
+        {searching && 
+        <div style={{height: "64px"}}>
+          <div className="loader"></div></div>}
       </form>
-      {searchProcess && <div>正在搜索： {searchProcess}</div>}
       <table>
         <tbody>
           {searchResult.map((x) => (

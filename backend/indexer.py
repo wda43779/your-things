a06228833eb1fbbbe30c9ccbc732f76e3aaaf0ea
wda43779 <wda43779@gmail.com>
2025-01-01 15:49:50 +0800

    update error import

--- backend/indexer.py
+++ backend/indexer.py
@@ -3,13 +3,13 @@ import sqlite3
 from constants import DB_PATH
 
 from wjw import get_all_files, words_and_re_sort
-from createdb import createdb
+from createdb import create_db
 import os
 
 
 def indexer():
     if not os.path.exists(DB_PATH):
-        createdb()  # 如果文件不存在，则调用 createdb 函数创建数据库
+        create_db()  # 如果文件不存在，则调用 createdb 函数创建数据库
     con = sqlite3.connect(DB_PATH)
     files_content = get_all_files(con, ".")
     words_and_re_sort(con, files_content, )

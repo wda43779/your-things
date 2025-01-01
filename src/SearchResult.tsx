9cbfed5fe1e3bfe35a33dc0c2589a425e3f2d42e
wda43779 <wda43779@gmail.com>
2025-01-01 08:06:46 +0800

    add types

---
+++ src/SearchResult.tsx
@@ -0,0 +1,11 @@
+interface SearchResult {
+  path: string;
+  name: string;
+  ext: string;
+  content: string;
+  createTime: string;
+  updateTime: string;
+  tags: string[];
+}
+
+export type { SearchResult };

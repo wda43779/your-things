174fd9e1c703d09c53ffd05dabeb5eb0b2a85c56
wda43779 <wda43779@gmail.com>
2025-01-01 17:06:48 +0800

    allow multiple words in search

--- backend/search.py
+++ backend/search.py
@@ -3,29 +3,48 @@ import json
 import sys
 import sqlite3
 from constants import DB_PATH
+import jieba
 
 
-def query_by_word(connection, word: str) -> list[dict]:
+def query_by_words(connection, words: str) -> list[dict]:
     cursor: Cursor = connection.cursor()
+
+    word_list = jieba.cut_for_search(words)
+    file_id_sets = []
+
+    for word in word_list:
+        cursor.execute(
+            """
+            SELECT re_sort.file_id
+            FROM re_sort
+            WHERE re_sort.word_id IN (
+                SELECT words.id
+                FROM words
+                WHERE words.word = ?
+            )
+            """,
+            (word,),
+        )
+        # 将查询结果转换为集合
+        file_ids = {row[0] for row in cursor.fetchall()}
+        file_id_sets.append(file_ids)
+
+    common_file_ids = set.intersection(*file_id_sets) if file_id_sets else set()
+
+    if not common_file_ids:
+        print("[]")
+    
+    # 查询交集文件 ID 对应的文件信息
     cursor.execute(
         """
         SELECT files.file_path, files.file_name, files.file_type, files.create_time, files.update_time, files.tag
-        FROM files 
-        WHERE files.id IN (
-            SELECT re_sort.file_id 
-            FROM re_sort 
-            WHERE re_sort.word_id IN (
-                SELECT words.id 
-                FROM words 
-                WHERE words.word = ?
-            )
-        )
-    """,
-        (word,),
+        FROM files
+        WHERE files.id IN ({})
+        """.format(",".join("?" * len(common_file_ids))),
+        tuple(common_file_ids),
     )
     result = cursor.fetchall()
 
-    # Map the result to a list of dictionaries
     search_results = []
     for row in result:
         file_loc = os.path.join(row[0])  # 构造文件的完整路径
@@ -44,12 +63,13 @@ def query_by_word(connection, word: str) -> list[dict]:
         }
         search_results.append(search_result)
 
+    # 打印 JSON 结果
     print(json.dumps(search_results, indent=2))
 
 
 def search(text):
     con = sqlite3.connect(DB_PATH)
-    query_by_word(con, text)
+    query_by_words(con, text)
 
 
 if __name__ == "__main__":
